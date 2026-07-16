dbd83bc49d313850b94e0699404c0371ad299a6f
Giorgi <giobuisness97@gmail.com>
2026-07-16 13:15:44 +0400

    Initial Commit

---
+++ Corner/CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CornerApp.swift
+//  Corner
+//
+//  Created by giorgi giorgadze on 16/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
